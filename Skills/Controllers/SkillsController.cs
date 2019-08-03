8504d35645cb681751318f4117c0bb21b93593bc
Evgeniy Poznyak <evgeniy.poznyak@gmail.com>
2019-08-03 12:47:00 -0500

    A little bit refactoring

--- Skills/Controllers/SkillsController.cs
+++ Skills/Controllers/SkillsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AccountDataExtractService.Logger;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Skills.Domain.Entity;
